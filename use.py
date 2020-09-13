bbbceb0522acd886270585619b84f9583c7fc43a
Vitaly Protasov <izakladno@yandex.ru>
2020-09-13 09:38:53 +0300

    fixed

--- use.py
+++ use.py
@@ -1,15 +1,6 @@
-import 
 import telebot
-import os
-import requests
-import use
-import re
-import numpy as np
-import time
-from oleg import *
-
-
 types = telebot.types
+
 button_zero = types.InlineKeyboardMarkup()
 btn0 = types.InlineKeyboardButton(text="Начать тест", callback_data='test')
 button_zero.row(btn0)
@@ -22,7 +13,7 @@ button_first.row(btn1)
 button_second = types.InlineKeyboardMarkup()
 button_second.row_width = 2
 btn2 = types.InlineKeyboardButton(text="Изучить материал", callback_data='read')
-btn3 = types.InlineKeyboardButton(text="Сдать экзамен по теме", callback_data='exam')
+btn3 = types.InlineKeyboardButton(text="Экзамен", callback_data='exam')
 btn4 = types.InlineKeyboardButton(text="Назад", callback_data='back')
 list_of_buttons = [btn2, btn3, btn4]
 for btn in list_of_buttons:
@@ -31,9 +22,3 @@ for btn in list_of_buttons:
 button_to_start = types.InlineKeyboardMarkup()
 btn5 = types.InlineKeyboardButton(text="Назад", callback_data='back2')
 button_to_start.row(btn5)
-
-
-if __name__ == '__main__':
-    token = ''
-    bot = OlegBot(token, )
-    bot.polling(none_stop=True)
